bdd55acd029ae9b00be13f924f940386a94a94e8
Denis-Yorsh <demis_my_mail.yahoo.com>
2023-11-21 19:21:53 +0200

    script is ready

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
-import "jquery-ui/dist/jquery-ui.min";
 import $ from "jquery";
+import "jquery-ui/dist/jquery-ui.min";
 
 $(() => {
+  const effectArray = [
+    "explode",
+    "bounce",
+    "puff",
+    "scale",
+    "size",
+    "blind",
+    "shake",
+    "pulsate",
+  ];
+  const random = () => Math.floor(Math.random() * (7 - 0 + 1) + 0);
   const main = $("#main");
-  $(main).toggle("blind", 2000);
 
-  $("a.asd").click((event) => {
+  $(main).toggle(effectArray[random()], 2000);
+
+  $("a").click((event) => {
     event.preventDefault();
-    console.log("click")
-    $(main).toggle("explode", 2000);
-    setTimeout(go, 1800);
+    $(main).toggle(effectArray[random()], 2000);
+    setTimeout(() => {
+      location.href = event.target.href;
+    }, 1800);
   });
-
-  const go = () => {
-    location.href = "https://www.google.com/";
-  };
 });
